c12a96232e8ffcdcfc284c29ddb2816fd1983efa
Kerawos <Maresowa@gmail.com>
2017-11-22 17:41:47 +0100

    @add img's

--- src/main/java/pl/akademiakodu/AK_Spring_Exercise_Restaurant/models/services/OrderObserver.java
+++
@@ -1,7 +0,0 @@
-package pl.akademiakodu.AK_Spring_Exercise_Restaurant.models.services;
-
-import pl.akademiakodu.AK_Spring_Exercise_Restaurant.models.Order;
-
-public interface OrderObserver {
-    void orderUpdate(Order order);
-}
